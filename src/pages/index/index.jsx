613a269d0f0d953427c21c44354ebe52647abacc
876657294@qq.com <876657294@qq.com>
2023-03-02 07:50:12 +0800

    new commit

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,11 @@
+
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+const Index = () => {
+  return (
+    <View><Text>Hello world</Text></View>
+  )
+}
+
+export default Index
